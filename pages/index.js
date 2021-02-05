9e5eb30b542ba1ca483cb096724fc6f92ba25c40
Lucas Palma Stabile <lucas.stabile93@gmail.com>
2021-02-05 13:50:21 -0300

    Alterações de cor, link de github e imagem em telas reduzidas

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Widget from '../src/components/Widget'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
+import Head from 'next/head'
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -18,27 +19,34 @@ const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground alt='Imagem de fundo cheio de pontos de interrogação' backgroundImage={db.bg}>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>Título1</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>parágrafo1</p>
-          </Widget.Content>
-        </Widget>
-        <Widget>
-          <Widget.Header>
-            <h1>Título2</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>parágrafo2</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl='https://github.com/Hilgo' />
-    </QuizBackground>
+    <>
+      <Head>
+        <title>Quiz Curiosidades - Lucas Palma Stabile</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <QuizBackground alt='Imagem de fundo cheio de pontos de interrogação' backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>Título1</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>parágrafo1</p>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Header>
+              <h1>Título2</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>parágrafo2</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl='https://github.com/Hilgo/quiz-curiosidades' />
+      </QuizBackground>
+    </>
   )
 }
--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -9,14 +9,14 @@ const QuizBackground = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   flex: 1;
   @media screen and (max-width: 500px) {
-    background-image: none;
+    //background-image: none;
     &:after {
       content: "";
       background-size: cover;
     background-position: center;
-      background-image:
-        linear-gradient(transparent, ${({ theme }) => theme.colors.mainBg}),
-        url(${({ backgroundImage }) => backgroundImage});
+      // background-image:
+      //   linear-gradient(transparent, ${({ theme }) => theme.colors.mainBg}),
+      //   url(${({ backgroundImage }) => backgroundImage});
       display: block;
       width: 100%;
       height: 210px;
