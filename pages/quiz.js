beceb87f2d54ea828cca1489e46e9aaed57795d0
Lucas Palma Stabile <lucas.stabile93@gmail.com>
2021-02-12 01:26:02 -0300

    Aula 4, inclusão de respostas e validação de acertos

--- pages/quiz.js
+++ pages/quiz.js
@@ -2,140 +2,233 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
-// import Footer from '../src/components/Footer';
-// import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
+import AlternativesForm from '../src/components/AlternativesForm';
 import Button from '../src/components/Button';
-// import Input from '../src/components/Input';
 
-function LoadingWidget() {
-  return (
-    <Widget>
-      <Widget.Header>Carregando...</Widget.Header>
-      <Widget.Content>[Desafio do loading]</Widget.Content>
-    </Widget>
-  );
-}
-
-function QuestionWidget({
-  pergunta, totalPerguntas, indicePergunta, AoEnviar,
-}) {
-  const IdPergunta = `pergunta__${indicePergunta}`;
+function ResultWidget({ results }) {
   return (
     <Widget>
       <Widget.Header>
-        <h3>
-          {`Pergunta ${indicePergunta + 1} de ${totalPerguntas}`}
-        </h3>
+        Tela de Resultado:
       </Widget.Header>
-      <img
-        alt="descrição"
-        style={{
-          width: '100%',
-          height: '150px',
-          objectFit: 'cover',
-        }}
-        src={pergunta.image}
-      />
+
       <Widget.Content>
-        <h2>
-          {pergunta.title}
-        </h2>
-        <p>{pergunta.description}</p>
-        <form onSubmit={(evento) => {
-          evento.preventDefault();
-          AoEnviar();
-        }}
-        >
-          {pergunta.alternatives.map((alternativa, alternativaIndex) => {
-            const idAlternativa = `alternativa__${alternativaIndex}`;
-            return (
-              <Widget.Topic
-                as="label"
-                htmlFor={idAlternativa}
-              >
-                <input
-                  id={idAlternativa}
-                  name={IdPergunta}
-                  type="radio"
-                />
-                {alternativa}
-              </Widget.Topic>
-            );
-          })}
-          {/* <pre>
-          {JSON.stringify(pergunta,null,4)}
-        </pre> */}
-          <Button type="submit">
-            Confirmar
-          </Button>
-        </form>
+        <p>
+          Você acertou
+          {' '}
+          {/* {results.reduce((somatoriaAtual, resultAtual) => {
+            const isAcerto = resultAtual === true;
+            if (isAcerto) {
+              return somatoriaAtual + 1;
+            }
+            return somatoriaAtual;
+          }, 0)} */}
+          {results.filter((x) => x).length}
+          {' '}
+          perguntas
+        </p>
+        <ul>
+          {results.map((result, index) => (
+            <li key={`result__${result}`}>
+              #
+              {index + 1}
+              {' '}
+              Resultado:
+              {result === true
+                ? 'Acertou'
+                : 'Errou'}
+            </li>
+          ))}
+        </ul>
       </Widget.Content>
     </Widget>
   );
 }
 
-const estadosDaTela = {
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  questionIndex,
+  totalQuestions,
+  onSubmit,
+  addResult,
+}) {
+  const [selectedAlternative, setSelectedAlternative] = React.useState(undefined);
+  const [isQuestionSubmited, setIsQuestionSubmited] = React.useState(false);
+  const questionId = `question__${questionIndex}`;
+  const isCorrect = selectedAlternative === question.answer;
+  const hasAlternativeSelected = selectedAlternative !== undefined;
+
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <AlternativesForm
+          onSubmit={(infosDoEvento) => {
+            infosDoEvento.preventDefault();
+            setIsQuestionSubmited(true);
+            setTimeout(() => {
+              addResult(isCorrect);
+              onSubmit();
+              setIsQuestionSubmited(false);
+              setSelectedAlternative(undefined);
+            }, 2 * 1000);
+          }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            const alternativeStatus = isCorrect ? 'SUCCESS' : 'ERROR';
+            const isSelected = selectedAlternative === alternativeIndex;
+            return (
+              <Widget.Topic
+                as="label"
+                key={alternativeId}
+                htmlFor={alternativeId}
+                data-selected={isSelected}
+                data-status={isQuestionSubmited && alternativeStatus}
+              >
+                <input
+                  style={{ display: 'none' }}
+                  id={alternativeId}
+                  name={questionId}
+                  onChange={() => setSelectedAlternative(alternativeIndex)}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          {/* <pre>
+            {JSON.stringify(question, null, 4)}
+          </pre> */}
+          <Button type="submit" disabled={!hasAlternativeSelected}>
+            Confirmar
+          </Button>
+          {isQuestionSubmited && isCorrect && <p>Você acertou!</p>}
+          {isQuestionSubmited && !isCorrect && <p>Você errou!</p>}
+        </AlternativesForm>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
   QUIZ: 'QUIZ',
-  CARREGANDO: 'CARREGANDO',
-  RESULTADO: 'RESULTADO',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
 };
+export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const [results, setResults] = React.useState([]);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
 
-export default function PaginaQuiz() {
-  const totalPerguntas = db.questions.length;
-  const [indicePergunta, atribuirIndicePergunta] = React.useState(0);
-  const pergunta = db.questions[indicePergunta];
-  const [estadoTela, atribuirEstado] = React.useState(estadosDaTela.CARREGANDO);
+  function addResult(result) {
+    // results.push(result);
+    setResults([
+      ...results,
+      result,
+    ]);
+  }
 
-  // [No React é chamado de Effects]
-
-  // cresce -> WillUpdate
-  // morre -> WilUnmount
+  // [React chama de: Efeitos || Effects]
+  // React.useEffect
+  // atualizado === willUpdate
+  // morre === willUnmount
   React.useEffect(() => {
-    // fetch...
-    setTimeout(() => { atribuirEstado(estadosDaTela.QUIZ); }, 1 * 1000);
-    // nasce -> didMount
+    // fetch() ...
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  // nasce === didMount
   }, []);
 
-  function TratarPerguntaAtual() {
-    const proximaPergunta = indicePergunta + 1;
-    if (proximaPergunta < totalPerguntas) {
-      atribuirIndicePergunta(proximaPergunta);
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
     } else {
-      atribuirEstado(estadosDaTela.RESULTADO);
+      setScreenState(screenStates.RESULT);
     }
   }
 
   return (
-    <QuizBackground alt="Imagem de fundo cheio de pontos de interrogação" backgroundImage={db.bg}>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        {/* <QuizLogo /> */}
-        {estadoTela === estadosDaTela.QUIZ && (
-        <QuestionWidget
-          pergunta={pergunta}
-          totalPerguntas={totalPerguntas}
-          indicePergunta={indicePergunta}
-          AoEnviar={TratarPerguntaAtual}
-        />
-        ) }
-        {estadoTela === estadosDaTela.CARREGANDO && <LoadingWidget /> }
-        {estadoTela === estadosDaTela.RESULTADO && <div>Você acertou X questões, parabéns!</div> }
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmitQuiz}
+            addResult={addResult}
+          />
+        )}
 
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <ResultWidget results={results} />}
       </QuizContainer>
     </QuizBackground>
   );
 }
 
 QuestionWidget.propTypes = {
-  pergunta: PropTypes.shape({
+  question: PropTypes.shape({
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string,
     answer: PropTypes.number.isRequired,
     alternatives: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
-  totalPerguntas: PropTypes.number.isRequired,
-  indicePergunta: PropTypes.number.isRequired,
-  AoEnviar: PropTypes.func.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  questionIndex: PropTypes.number.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  addResult: PropTypes.func.isRequired,
+};
+
+ResultWidget.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.bool).isRequired,
 };
